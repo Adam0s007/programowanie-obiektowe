a4fc9948f6f8786cff0a7ed8643e51e6c0521a67
Adam Biśta <adamosekk2002@gmail.com>
2022-11-12 20:02:09 +0100

    minor changes

---
+++ oolab/src/test/java/agh/ics/oop/RectangularMapTest.java
@@ -0,0 +1,63 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangularMapTest {
+    String[] arr = {"f", "l", "r","f","f"}; //argumenty dobrane tak, aby doszlo do zderzenia lub unikniecia kolizji
+    MoveDirection[] directions = new OptionsParser().parse(arr);
+    IWorldMap mapa = new RectangularMap(16, 16);
+    Vector2d[] posits = { new Vector2d(1,2), new Vector2d(2, 3)};//argumenty dobrane tak, aby doszlo do zderzenia lub unikniecia kolizji
+    IEngine engine = new SimulationEngine(directions, mapa, posits);
+
+    @Test
+    void canMoveTo() {
+        engine.run();
+        assertTrue(mapa.canMoveTo(new Vector2d(2, 2)));
+        assertFalse(mapa.canMoveTo(new Vector2d(2, 3)));
+        assertFalse(mapa.canMoveTo(new Vector2d(1, 3)));
+    }
+
+    @Test
+    void place() {
+        engine.run();
+        Animal zwierzak = new Animal(mapa, new Vector2d(2, 2));
+        Animal zwierzak1 = new Animal(mapa, new Vector2d(1, 3));
+        assertTrue(mapa.place(zwierzak)); //doda nowe zwierzę!
+        System.out.println(mapa.toString());
+        assertFalse(mapa.place(zwierzak1));
+        System.out.println(mapa.toString());
+        //dzieki fladze started w klasie SimulationEngine mozemy dynamicznie dodawac nowe zwierzeta
+        engine.run();
+        //ponizej zastosowano przetestowanie zaistnienia nowego zwierzątka na mapie:
+        RectangularMap myMap = (RectangularMap) mapa;
+        assertEquals(mapa.objectAt(new Vector2d(1, 3)), myMap.getAnimals().get(0));
+        assertEquals(mapa.objectAt(new Vector2d(2, 3)), myMap.getAnimals().get(1));
+        assertEquals(mapa.objectAt(new Vector2d(2, 2)), myMap.getAnimals().get(2));
+
+    }
+
+    @Test
+    void isOccupied() {
+        engine.run();
+        assertFalse(mapa.isOccupied(new Vector2d(2, 2)));
+        assertTrue(mapa.isOccupied(new Vector2d(2, 3)));
+        assertTrue(mapa.isOccupied(new Vector2d(1, 3)));
+    }
+
+    @Test
+    void objectAt() {
+        engine.run();
+        RectangularMap myMap = (RectangularMap) mapa;
+        ArrayList<Animal> animals = myMap.getAnimals();
+
+        assertNotEquals(mapa.objectAt(new Vector2d(2, 3)), animals.get(0));
+        assertEquals(mapa.objectAt(new Vector2d(1, 3)), animals.get(0));
+        assertEquals(mapa.objectAt(new Vector2d(2, 3)), animals.get(1));
+        assertEquals(mapa.objectAt(new Vector2d(0, 0)),null);
+
+    }
+}
---
+++ oolab/src/test/java/agh/ics/oop/SimulationEngineTest.java
@@ -0,0 +1,57 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationEngineTest {
+
+
+    @Test
+    void run() {
+        //sprawdzanie poprawnosci w przemieszczaniu sie
+        String[] arr = {"f", "l", "r","f","f"};
+        MoveDirection[] directions = new OptionsParser().parse(arr);
+        IWorldMap mapa = new RectangularMap(16, 16);
+        Vector2d[] posits = { new Vector2d(1,2), new Vector2d(2, 3)};
+        SimulationEngine engine = new SimulationEngine(directions, mapa, posits);
+        engine.appendAnimals();//uwaga! metoda jest wywolywana tylko raz, wywolywana automatycznie po wywolaniu run()
+        List<Animal> animals = ((RectangularMap) mapa).getAnimals();
+
+        assertEquals(animals.get(0).getPosition(), new Vector2d(1,2));
+        assertEquals(animals.get(1).getPosition(), new Vector2d(2,3));
+
+        //sprawdzanie poprawnosci w przemieszczaniu sie
+        String[] arr2 = {"f", "f", "f", "f","b","b","l","f","f"};
+        MoveDirection[] directions2 = new OptionsParser().parse(arr2);
+        IWorldMap mapa2 = new RectangularMap(16, 16);
+        Vector2d[] posits2 = { new Vector2d(2,2), new Vector2d(2, 3), new Vector2d(2,2)};
+        IEngine engine2 = new SimulationEngine(directions2, mapa2, posits2);
+        engine2.run();
+        List <Animal> animals2 = ((RectangularMap) mapa2).getAnimals();
+
+        assertEquals(animals2.get(0).getPosition(), new Vector2d(1,2));
+        assertEquals(animals2.get(1).getPosition(), new Vector2d(2,5));
+
+
+        //sprawdzanie czy zwierzątka wyjdą poza mapę, ich pozycja koncowa, ich kierunek
+        String[] arr3 = {"f","b","b","l","r","b","f","f","f","f"};
+        MoveDirection[] directions3 = new OptionsParser().parse(arr3);
+        IWorldMap mapa3 = new RectangularMap(16, 16);
+        Vector2d[] posits3 = { new Vector2d(1,2), new Vector2d(2, 3), new Vector2d(3,1)};
+        IEngine engine3 = new SimulationEngine(directions3, mapa3, posits3);
+        engine3.run();
+
+        List <Animal> animals3 = ((RectangularMap) mapa3).getAnimals();
+
+        assertEquals(animals3.get(0).getPosition(), new Vector2d(0,3));
+        assertEquals(animals3.get(1).getPosition(), new Vector2d(3,2));
+        assertEquals(animals3.get(2).getPosition(), new Vector2d(3,1));
+        assertEquals(animals3.get(0).getDirection(), MapDirection.WEST);
+        assertEquals(animals3.get(1).getDirection(), MapDirection.EAST);
+        assertEquals(animals3.get(2).getDirection(), MapDirection.NORTH);
+
+    }
+}
